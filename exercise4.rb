cd2bf0fc30c35141b8cb46efc546bb749ed2fcee
kano <kano@throo.io>
2023-07-03 17:37:09 +0900

    課題74の修正

--- exercise4.rb
+++ exercise4.rb
@@ -1,26 +1,27 @@
+# 第1引数が与えられていなければエラーを出す
+if !ARGV[0]
+    raise "引数を与えてください。"
+end
+
 # コマンドライン引数に"rooms"が与えられているか確認
 arg_rooms = ARGV[0] == "rooms"
 
-# コマンドライン引数に"posts"が与えられているか確認する。与えられていたら部屋IDを確認し、グローバル変数room_idに保存する
+# コマンドライン引数に"posts"が与えられているか確認
 arg_posts = ARGV[0] == "posts"
-if arg_posts
-    $room_id = ARGV[1]
-    if ! $room_id
-        raise "部屋IDを与えてください。"
-    end
+
+# コマンドライン引数に"submit-post"が与えられているか確認
+arg_submit = ARGV[0] == "submit-post"
+
+# 部屋idを確認(3パターンで共通)
+room_id = ARGV[1]
+if !arg_rooms && !room_id
+    raise "部屋idを与えてください。"
 end
 
-# コマンドライン引数に"submit-post"が与えられているか確認し、与えられていたら内容も確認し、グローバル変数contentに保存する
-arg_submit = ARGV[0] == "submit-post"
-if arg_submit
-    $room_id = ARGV[1]
-    $content = ARGV[2]
-    if ! $room_id
-        raise "部屋IDを与えてください。"
-    end
-    if ! $content
-        raise "投稿の内容を与えてください。" 
-    end
+# 投稿の内容を確認
+content = ARGV[2]
+if arg_submit && !content
+    raise "投稿の内容を入力してください。"
 end
 
 # 標準ライブラリであるjsonをrequire
@@ -36,20 +37,21 @@ response = Faraday.get("https://next-chat-kohl.vercel.app/api/room_ids")
 # response.bodyはJSON文字列なので、JSON.parseで文字列からrubyのハッシュに変換する
 room_ids = JSON.parse(response.body)
 
-# コマンドライン引数に"posts"または"submit-post"が与えられていなければroom_idsを表示
-if ! arg_posts && ! arg_submit
+# コマンドライン引数に"rooms"が与えられていればroom_idsを表示
+if arg_rooms
     puts room_ids
 end
 
-# コマンドライン引数に"rooms"または"submit-post"が与えられていなければ投稿を表示
-if ! arg_rooms && ! arg_submit
-    response = Faraday.get("https://next-chat-kohl.vercel.app/api/posts", room_id: $room_id)
+# コマンドライン引数に"posts"が与えられていれば投稿を表示
+if arg_posts
+    response = Faraday.get("https://next-chat-kohl.vercel.app/api/posts", room_id: room_id)
     posts = JSON.parse(response.body)
     puts posts
 end
 
-# コマンドライン引数に"rooms"または"posts"が与えられていなければ投稿を送信
-if ! arg_rooms && ! arg_posts
-    Faraday.post("https://next-chat-kohl.vercel.app/api/posts", "{\"roomId\": #{$room_id},\"content\": #{$content}", "Content-Type" => "application/json")
-    puts "投稿を送信しました。部屋ID：「#{$room_id}」、内容：「#{$content}」"
+# コマンドライン引数に"submit-post"が与えられていれば投稿を送信
+if arg_submit
+    json_str = "{\"roomId\": \"#{room_id}\",\"content\": \"#{content}\"}"
+    res = Faraday.post("https://next-chat-kohl.vercel.app/api/posts", json_str, "Content-Type" => "application/json")
+    puts "投稿を送信しました。部屋ID：「#{room_id}」、内容：「#{content}」"
 end
